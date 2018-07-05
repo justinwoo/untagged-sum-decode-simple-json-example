72d6c6ddf112bceb5eeda42283493d0bed5e8ba3
justinwoo <moomoowoo@gmail.com>
2018-07-05 23:00:31 +0300

    update for 0.12

--- src/Main.purs
+++ src/Main.purs
@@ -3,16 +3,16 @@ module Main where
 import Prelude
 
 import Control.Alt ((<|>))
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log, logShow)
 import Data.Either (Either)
-import Data.Foreign (ForeignError)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
 import Data.List.NonEmpty (NonEmptyList)
-import Data.StrMap (StrMap)
+import Effect (Effect)
+import Effect.Console (logShow)
+import Foreign (ForeignError)
+import Foreign.Object (Object)
 import Global.Unsafe (unsafeStringify)
-import Simple.JSON (class ReadForeign, read, readJSON)
+import Simple.JSON (class ReadForeign, read', readJSON)
 
 type NumberValueRecord =
   { a :: Int
@@ -28,7 +28,7 @@ data PossibleValues
   = NumberValue NumberValueRecord
   | StringValue StringValueRecord
 instance readForeignPossibleValues :: ReadForeign PossibleValues where
-  readImpl f = NumberValue <$> read f <|> StringValue <$> read f
+  readImpl f = NumberValue <$> read' f <|> StringValue <$> read' f
 
 -- some stuff for generic Show of PossibleValues
 derive instance genericPossibleValues :: Generic PossibleValues _
@@ -70,15 +70,16 @@ testJson3 = """
 decodeJSON :: String -> Either (NonEmptyList ForeignError) PossibleValues -- usually you provide this type through usage or scoped type variables
 decodeJSON = readJSON
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+main :: Effect Unit
 main = do
   logShow $ decodeJSON testJson1
   logShow $ decodeJSON testJson2
-  logShow $ (readJSON testJson3 :: Either (NonEmptyList ForeignError) (StrMap PossibleValues)) -- context provided like so
-  log <<< unsafeStringify $ (readJSON testJson3 :: Either (NonEmptyList ForeignError) (StrMap PossibleValues)) -- yolo, just JSON.stringify
+  logShow $ (readJSON testJson3 :: Either (NonEmptyList ForeignError) (Object PossibleValues)) -- context provided like so
+  logShow <<< unsafeStringify $ (readJSON testJson3 :: Either (NonEmptyList ForeignError) (Object PossibleValues)) -- just as the raw form
 
   -- output:
-  -- (Right (NumberValue { a: 1, b: 123 })) <-- as expected
-  -- (Right (StringValue { a: 1, b: "asdf" })) <-- also as expected
-  -- (Right (fromFoldable [(Tuple "A1" (NumberValue { a: 1, b: 123 })),(Tuple "B1" (StringValue { a: 1, b: "asdf" }))])) <-- yup, a stringmap stringified is basically a bunch of tuples
-  -- {"value0":{"A1":{"value0":{"b":123,"a":1}},"B1":{"value0":{"b":"asdf","a":1}}}}"
+  -- (Right (NumberValue { a: 1, b: 123 }))
+  -- (Right (StringValue { a: 1, b: "asdf" }))
+  -- (Right (fromFoldable [(Tuple "A1" (NumberValue { a: 1, b: 123 })),(Tuple "B1" (StringValue { a: 1, b: "asdf" }))]))
+  -- "{\"value0\":{\"A1\":{\"value0\":{\"b\":123,\"a\":1}},\"B1\":{\"value0\":{\"b\":\"asdf\",\"a\":1}}}}"
+
